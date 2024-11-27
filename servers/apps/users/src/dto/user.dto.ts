13336d3e88a569a2bc7d33a878dc5b03a1743c2c
eduardo <eduardomendes418@gmail.com>
2024-11-27 19:19:31 -0300

    [MFD-12] feat:  add  dto

---
+++ servers/apps/users/src/dto/user.dto.ts
@@ -0,0 +1,19 @@
+import { InputType, Field} from  '@nestjs/graphql'
+import { IsEmail, IsNotEmpty, IsString, MinLength} from 'class-validator'
+
+@InputType()
+export class RegisterDto{
+    @Field()
+    @IsNotEmpty({ message: 'Name is required.'})
+    @IsString({ message: 'Name must need to be one string'})
+    name: string
+
+    @Field()
+    @IsNotEmpty({message: 'Password is required'})
+    @MinLength(8, {message: 'Password must be at at least 8 characters'})
+
+    @Field()
+    @IsNotEmpty({ message: 'Email is required'})
+    @IsEmail({}, {message: 'Email is Invalid'})
+    email: string
+}
