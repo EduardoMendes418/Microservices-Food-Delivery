98aba623987500ae11e2701af6bcc6b407eb39e9
eduardo <eduardomendes418@gmail.com>
2024-11-27 19:29:55 -0300

    [MFD-13] fix:  gateway modules

--- servers/apps/gateway/src/app.module.ts
+++ servers/apps/gateway/src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppService } from './app.service';
-
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloGatewayDriver, ApolloGatewayDriverConfig } from '@nestjs/apollo';
+import { IntrospectAndCompose } from '@apollo/gateway'
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloGatewayDriverConfig>({
+      driver: ApolloGatewayDriver,
+      gateway: {
+        supergraphSdl: new IntrospectAndCompose({
+          subgraphs: [],
+        })
+      }
+    })
+  ],
   controllers: [],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
