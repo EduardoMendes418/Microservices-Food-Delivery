6b45f430d22eec743161bd451c014f3a9063ea3c
eduardo <eduardomendes418@gmail.com>
2024-11-28 07:30:36 -0300

    [MFD-17] feat:  create register user service

--- servers/apps/users/src/users.service.ts
+++ servers/apps/users/src/users.service.ts
@@ -1,8 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
+import { RegisterDto } from './dto/user.dto';
 
 @Injectable()
 export class UsersService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(
+    private readonly JwtService: JwtService,
+    //private readonly
+    private readonly configService: ConfigService,
+  ) { }
+
+  // register user service
+  async register(registerDto: RegisterDto){
+    const {name, email, password} =  registerDto;
+    const user = {
+      name,
+      email,
+      password,
+    };
+    return user;
   }
 }
