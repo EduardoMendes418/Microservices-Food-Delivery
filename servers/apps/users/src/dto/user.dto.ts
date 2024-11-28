af5a669ca7553c1ae3fe3dac81b7ea8517804efc
eduardo <eduardomendes418@gmail.com>
2024-11-28 07:39:44 -0300

    [MFD-20] feat:  create LoginDto

--- servers/apps/users/src/dto/user.dto.ts
+++ servers/apps/users/src/dto/user.dto.ts
@@ -1,20 +1,35 @@
-import { InputType, Field} from  '@nestjs/graphql'
-import { IsEmail, IsNotEmpty, IsString, MinLength} from 'class-validator'
+import { InputType, Field } from '@nestjs/graphql'
+import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator'
 
 @InputType()
-export class RegisterDto{
+export class RegisterDto {
     @Field()
-    @IsNotEmpty({ message: 'Name is required.'})
-    @IsString({ message: 'Name must need to be one string'})
+    @IsNotEmpty({ message: 'Name is required.' })
+    @IsString({ message: 'Name must need to be one string' })
     name: string
 
     @Field()
-    @IsNotEmpty({message: 'Password is required'})
-    @MinLength(8, {message: 'Password must be at at least 8 characters'})
+    @IsNotEmpty({ message: 'Password is required' })
+    @MinLength(8, { message: 'Password must be at at least 8 characters' })
     password: string
 
     @Field()
-    @IsNotEmpty({ message: 'Email is required'})
-    @IsEmail({}, {message: 'Email is Invalid'})
+    @IsNotEmpty({ message: 'Email is required' })
+    @IsEmail({}, { message: 'Email is Invalid' })
     email: string
+}
+
+
+@InputType()
+export class LoginDto {
+    @Field()
+    @IsNotEmpty({ message: 'Email is required' })
+    @IsEmail({}, { message: 'Email is Invalid' })
+    email: string
+
+    @Field()
+    @IsNotEmpty({ message: 'Password is required' })
+    @MinLength(8, { message: 'Password must be at at least 8 characters' })
+    password: string
+
 }
